7dd695ce79a08d11a9187f656a36b39db6c1dee3
WoutFirlefyn <firlefyn.wout@gmail.com>
2024-12-11 23:27:05 +0100

    Feat: Added fade in/out effect for project list

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ menuIcon.addEventListener("click", () => {
         body.style.overflowY = "auto";
     }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.project_container');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+                if (window.innerHeight - entry.target.getBoundingClientRect().bottom < 100) {
+                    entry.target.classList.add('bottom');
+                }
+                else {
+                    entry.target.classList.add('top');
+                }
+            }
+        });
+    }, { threshold: 0.7 });
+
+    sections.forEach(section => observer.observe(section));
+});
