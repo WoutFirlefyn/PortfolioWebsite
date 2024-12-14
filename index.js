ef428ce87d064962eb86473509a3a63b50764951
WoutFirlefyn <firlefyn.wout@gmail.com>
2024-12-14 16:44:19 +0100

    Feat: About me button scrolls to about me section

--- index.js
+++ index.js
@@ -47,3 +47,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+document.querySelector('.hero-button').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const target = document.getElementById('about');
+    const offset = 200;
+
+    const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+    });
+});
